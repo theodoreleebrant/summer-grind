08dd3e9cd18af3fbfbc665d8a47e03f809f38a67
theodoreleebrant@gmail.com <theodoreleebrant@gmail.com>
2022-05-12 01:52:57 +0800

    Add extra explanation

--- leetcode/1641-count-sorted-vowel-strings.py
+++ leetcode/1641-count-sorted-vowel-strings.py
@@ -4,7 +4,10 @@
 # A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
 
 # Iterative idea in the java solution.
-# Math solution: this is (n+4) choose 4
+# Math solution: this is (n+4) choose 4 (multiset problem)
+# Let's say for 5 letters, we have chosen 2,1,0,0,2.
+# This can only correspond to aaeuu, due to the lexicographic ordering restriction
+# Therefore we have n stars and 4 bars => n+4 choose 4.
 
 class Solution:
     def countVowelStrings(self, n: int) -> int:
